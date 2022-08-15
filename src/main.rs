2d2d344d9d5225320b0fcc13e345f4e0f4e0cdb6
n0s4 <joemckay3006@gmail.com>
2022-08-15 12:43:27 +0100

    Fix laggy input with tick timer.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,44 @@
 use macroquad::prelude::*;
 
-use std::{thread::sleep, time::Duration};
+const BACKGROUND: Color = DARKGREEN;
+const SNAKE_COLOUR: Color = GREEN;
 
+/// Size of a square on the game "grid".
 const SQUARE_SIZE: f32 = 50.0;
-/// Time to wait between each frame.
-const WAIT_TIME: Duration = Duration::from_millis(300);
+
+/// Time to wait between each tick, in seconds.
+const TICK_TIME: f32 = 0.3;
 
 #[macroquad::main("Snake")]
 async fn main() {
     // Move direction starts at 1 on the X axis, i.e going right.
     let mut move_direction = Vec2::X;
+
     // Initialise position to the center of the screen.
     let mut pos = Vec2::new(
         (screen_width() / 2.0) - (SQUARE_SIZE / 2.0),
         (screen_height() / 2.0) - (SQUARE_SIZE / 2.0),
     );
 
-    loop {
-        clear_background(DARKGREEN);
+    // Keeps track of how much time is left till the next tick (when the snake moves).
+    let mut timer: f32 = 0.0;
 
-        info!("position: {}", pos);
-        info!("move direction: {}", move_direction);
+    loop {
+        clear_background(BACKGROUND);
 
         // Set the move direction if a new input occured.
         if let Some(new_dir) = get_last_key_pressed().and_then(key_to_direction) {
             move_direction = new_dir;
         }
 
-        pos += move_direction * SQUARE_SIZE;
+        timer += get_frame_time();
+        // Move the snake if a tick has passed.
+        if timer > TICK_TIME {
+            timer = 0.0;
+            pos += move_direction * SQUARE_SIZE;
+        }
 
-        draw_rectangle(
-            pos.x,       // top left corner x
-            pos.y,       // top left corner y
-            SQUARE_SIZE, // width
-            SQUARE_SIZE, // height
-            GREEN,       // colour
-        );
-
-        // wait for a moment between each frame.
-        sleep(WAIT_TIME);
+        draw_rectangle(pos.x, pos.y, SQUARE_SIZE, SQUARE_SIZE, SNAKE_COLOUR);
 
         next_frame().await
     }
@@ -52,6 +52,7 @@ async fn main() {
 /// 'l' => [1, 0] // right
 /// ```
 fn key_to_direction(key: KeyCode) -> Option<Vec2> {
+    // Supports hjkl, arrow keys, and wasd.
     match key {
         KeyCode::H | KeyCode::Left | KeyCode::A => Some(-Vec2::X), // left
         KeyCode::J | KeyCode::Down | KeyCode::S => Some(Vec2::Y),  // down
